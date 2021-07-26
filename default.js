86d7140396ce8fe0c67ceefb94206b5ed515c9e0
Josh Koppen <joshuwakoppen@gmail.com>
2021-07-26 11:29:39 -0400

    Added display game result

--- default.js
+++ default.js
@@ -1,22 +1,13 @@
 
 let playerScore = 0;
 let computerScore = 0;
-let playerSelection;
-
-//Start a round when user makes a pick
-const buttons = document.querySelectorAll('button')
-	buttons.forEach((button) => {
-		button.addEventListener('click', () => {
-			
-				playRound(button.id);
-		})
-		
-	});
 
 
 
 
-//PC makes a selection
+
+
+
 function computerPlay() {
 		
     let randomNumber = Math.floor(Math.random()* 3);
@@ -30,75 +21,103 @@ function computerPlay() {
 }
 
 
+function scoreUpdate() {
 
-
+	const userScoreUpdate = document.getElementById('player');
+	const computerScoreUpdate = document.getElementById('computer');
 	
+	userScoreUpdate.textContent = ` ${playerScore} `;
+	computerScoreUpdate.textContent = ` ${computerScore} `;
+	
+	}
 
 
-// Compares choices to determine outcome and proper notification
-
-	function playRound(playerSelection) {
+function playRound(playerSelection) {
 		
-		computerSelection = computerPlay();
+	computerSelection = computerPlay();
 
-		const showResult = document.querySelector('#result');
+	const showResult = document.querySelector('#result');
 		
-			
+	
 		
 
-		if ((computerSelection == "rock" && playerSelection == "scissors")||
-			(computerSelection == "paper" && playerSelection =="rock")||
-			(computerSelection == "scissors" && playerSelection == "paper")) {
+	if ((computerSelection == "rock" && playerSelection == "scissors") ||
+		(computerSelection == "paper" && playerSelection =="rock") ||
+		(computerSelection == "scissors" && playerSelection == "paper")) {
 		
-			showResult.textContent =  ` ${computerSelection} beats ${playerSelection}. The computer wins! `; 
-			
-			return computerScore++;
+		showResult.textContent =  ` ${computerSelection} beats ${playerSelection}. The computer wins! `; 
+		computerScore++;
+		scoreUpdate();
+		checkScore();
+		
+		
+		
 
 		}
 
-		else if ((computerSelection == "rock" && playerSelection == "paper")||
-			(computerSelection == "paper" && playerSelection =="scissors")||
-			(computerSelection == "scissors" && playerSelection == "rock")){
-
+		else if ((computerSelection == "rock" && playerSelection == "paper") ||
+				(computerSelection == "paper" && playerSelection =="scissors") ||
+				(computerSelection == "scissors" && playerSelection == "rock")) {
 		
 			showResult.textContent = ` ${playerSelection} beats ${computerSelection}. You win! `;
-			return playerScore++;
+			playerScore++;
+			scoreUpdate();
+			checkScore();
+			
+			
+			
 		}
 
-		else if (computerSelection == playerSelection){
-
+		else if (computerSelection == playerSelection) {
+			
 			showResult.textContent = `${playerSelection} versus ${computerSelection}. Tie Game `;
-			return "TIE";
+			
+
 		}
 		
 		else {
+
 			alert( " Something didn't go right, select again " );
+
 			return playRound();
 		}
-		
-			}
-        
-    
-		
-		
-		
-		 
+
 	
-// When round limit is met, this returns the final tally
-function checkScore () {
-	if(computerScore > playerScore) {
-		return "You lose! " + computerScore + " to " + playerScore;
-	}
-
-	else if (playerScore > computerScore) {
-		return "You Win! " + playerScore + " to " + computerScore;
-	}
-
-	else {
-	return "Tie Game! " + playerScore + " to " + computerScore;
-	}
-
+		
 }
+        
+//Start a round when user makes a pick
+const buttons = document.querySelectorAll('button')
+	buttons.forEach((button) => {
+		button.addEventListener('click', () => {
+			
+				playRound(button.id);
+		})
+		
+	});
+
+function checkScore() {
+
+	const endGameResult = document.querySelector('#endResult');
+	
+		if(computerScore >= 5 ) {
+
+			buttons.removeEventListener('click', playRound );
+			endGameResult.textContent = "You Lose" ;
+		}
+
+		else if (playerScore >= 5 ) {
+
+			buttons.removeEventListener('click', playRound); 
+			endGameResult.textContent = "You Win" ;
+		}
+
+		else {
+			return
+		}
+	
+}
+	
 
 
-//Current state of things: I can call playRound, but my onClick is always scissors and I need to correct score keeping
+
